de98763fe27167ab04b76d0532266f6d369517af
Quan Tran <caoquantran2004@gmail.com>
2024-04-04 11:17:26 -0700

    update api key

--- src/ai_prompt/ai_prompt.module.ts
+++ src/ai_prompt/ai_prompt.module.ts
@@ -3,9 +3,10 @@ import { AiPromptController } from './ai_prompt.controller';
 import { AiPromptService } from './ai_prompt.service';
 import { UserModule } from 'src/user/user.module';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, HttpModule],
+  imports: [UserModule, HttpModule, ConfigModule],
   controllers: [AiPromptController],
   providers: [AiPromptService],
 })
--- src/ai_prompt/ai_prompt.service.ts
+++ src/ai_prompt/ai_prompt.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { GenerateStoryDto } from './dto/generateStory.dto';
 import { HttpService } from '@nestjs/axios';
 import { first, firstValueFrom, lastValueFrom, map } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AiPromptService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
   async getPrompt(data: GenerateStoryDto) {
     const axiousConfig = {
       headers: {
         'Content-Type': 'application/json',
         Authorization:
-          'Bearer ' +
-          'eyJhbGciOiJSUzI1NiIsImtpZCI6IjJDRDdCMUEwQTlBRTE1Q0VBRDBFNDJGNTFDQUU1Mzk5RjRBRTY5MjAiLCJ0eXAiOiJKV1QifQ.eyJjYW5SZWZyZXNoIjoiRmFsc2UiLCJ3b3Jrc3BhY2VJZCI6ImIxNmU5OGE2LTQ3Y2EtNDJiZS04MzJlLTM3ZTAzZDJlNjFkYiIsInRpZCI6IjgzMjJjZWZkLTBhNGMtNGUyYy1iZGU1LWIxNzkzM2U3YjAwZiIsIm9pZCI6ImExODQxMGIxLTI5MmQtNDE4MC04MDYzLTZjYjgxMTA4NjMzZCIsImFjdGlvbnMiOiJbXCJNaWNyb3NvZnQuTWFjaGluZUxlYXJuaW5nU2VydmljZXMvd29ya3NwYWNlcy9vbmxpbmVFbmRwb2ludHMvc2NvcmUvYWN0aW9uXCJdIiwiZW5kcG9pbnROYW1lIjoidGVybS1wcm9qZWN0LXdpamRkIiwic2VydmljZUlkIjoidGVybS1wcm9qZWN0LXdpamRkIiwiZXhwIjoxNzEyMDMxNjI4LCJpc3MiOiJhenVyZW1sIiwiYXVkIjoiYXp1cmVtbCJ9.b3_4BFDsiQBoR5saztEfSJswqC5esbIpNcTCx0IEX7BaAapO4ypHZ00-caYBwdAGV8PwbbSsYwR9h5MHfchiZS7hyKqKJUlorl1Imi9S_pw_1Xv0BOMEvQ2kJqNdRzmEiVxE-JtplHivsSoAF92ay5qw25338bq8WAq55tr_l9xWhBJhXB82aiVw3KENFQBYrZja309zAk-mUvPCIt2dFxNBznjEowpcwMz3gawlDpV13E7pvpzZwWPSt7MMr3w1hEFxFxNV01ZIeyEoj4KmHZWAmMcUbOuRLaqUd8H6txVJLNgDnobVabTFNlGNnH6IfRIyWkAB1ez5jYCqr8NpEw',
+          'Bearer ' + this.configService.get<string>('AZURE_ML_API_KEY'),
         'azureml-model-deployment': 'pranavpsv-gpt2-genre-story-ge-3',
       },
     };
